44cdaa6c385bba70a744bff8cf1bf9f2b5df790e
minnie thuo <111052951+minnieme20@users.noreply.github.com>
2023-06-12 13:15:31 +0300

    Add files via upload
    My black jack game using HTML, CSS and JavaScript

---
+++ black jack/index.js
@@ -0,0 +1,67 @@
+let player = {
+    name:"Minnie",
+    chips: 145
+}
+let cards = []  
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById ("message-el")
+let sumEl = document.getElementById ("sum-el")
+let cardsEl = document.getElementById ("cards-el")
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name +": $"+ player.chips
+
+function getRandomCard ( ) {
+    let randomCard = Math.floor(Math.random() * 13) + 1
+
+    if (randomCard === 1) {
+        return 11
+    }
+    else if (randomCard > 10) {
+        return 10
+     }
+     return randomCard
+}
+
+function startGame() {
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]  
+    sum = firstCard + secondCard
+    isAlive = true
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: " 
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum === 21) {
+        message = "Congratulations! You've got Blackjack!"
+        hasBlackJack = true
+    }
+    else {
+        message = "oops! You're out of the game!"
+        isAlive = false
+    } 
+    messageEl.textContent = message
+}
+
+
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
+        sum  += card
+        cards.push(card)
+        renderGame()
+    }
+    
+}
